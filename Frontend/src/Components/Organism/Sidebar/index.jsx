fc3cbc1c4d3a241cd5d19de050aa1979cb666c6b
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-08-15 11:53:53 +0700

    remove logo

--- Frontend/src/Components/Organism/Sidebar/index.jsx
+++ Frontend/src/Components/Organism/Sidebar/index.jsx
@@ -9,10 +9,10 @@ function Sidebar() {
         <>
             <div className="sidebar">
                 <div className="logo">
-                    <Link to={"/"}>
+                    {/* <Link to={"/"}>
                         <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png"
                             alt="Logo" />
-                    </Link>
+                    </Link> */}
                 </div>
                 <div className="navigation">
                     <ul>
